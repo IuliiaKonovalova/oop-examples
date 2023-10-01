48632ddd9761af03094239cc54d8ea98c6257bdb
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-10-01 15:08:23 -0500

    Add: methods to Course class

--- univ.py
+++ univ.py
@@ -21,3 +21,18 @@ class Course:
         self.name = name
         self.max_students = max_students
         self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+
+    def get_average_grade(self):
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+
+        return value / len(self.students)
+
+
